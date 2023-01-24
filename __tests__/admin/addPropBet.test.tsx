9f5c15116467a6bdcf2f33dba50fdc78f09d4a6e
Patrick Nance <jpnance@gmail.com>
2023-01-24 11:51:33 -0800

    Adding some simple form validation.

--- __tests__/admin/addPropBet.test.tsx
+++ __tests__/admin/addPropBet.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import AddPropBet from '@/pages/admin/addPropBet';
+import userEvent from '@testing-library/user-event';
 
 describe('/admin/addPropBet', () => {
 	it('should present a form for adding a new prop bet into the system', () => {
@@ -13,5 +14,40 @@ describe('/admin/addPropBet', () => {
 		};
 
 		const submitButton = screen.getByRole('button', { name: /Add Prop Bet/i });
+	});
+
+	it('should not allow the submit button to be clicked until the necessary fields have been provided', async () => {
+		const user = userEvent.setup();
+
+		render(<AddPropBet />);
+
+		const formFields = {
+			description: screen.getByRole('textbox', { name: /^Description$/i }),
+			shortDescription: screen.getByRole('textbox', { name: /Short Description/i }),
+			sides: screen.getAllByRole('textbox', { name: /Side/i }),
+			notes: screen.getByRole('textbox', { name: /Notes/i }),
+		};
+
+		const submitButton = screen.getByRole('button', { name: /Add Prop Bet/i });
+
+		expect(submitButton).toBeDisabled();
+
+		await user.type(formFields.notes, 'Whatever the box score says at 9am PT on Monday, February 13 is what we\'ll go with.');
+		expect(submitButton).toBeDisabled();
+
+		await user.type(formFields.description, 'How many total points will be scored?');
+		expect(submitButton).toBeDisabled();
+
+		await user.type(formFields.shortDescription, 'Total points');
+		expect(submitButton).toBeDisabled();
+
+		await user.type(formFields.sides[0], '49 or more');
+		expect(submitButton).toBeDisabled();
+
+		await user.type(formFields.sides[1], '48 or fewer');
+		expect(submitButton).toBeEnabled();
+
+		await user.clear(formFields.notes);
+		expect(submitButton).toBeEnabled();
 	});
 });
--- pages/admin/addPropBet.tsx
+++ pages/admin/addPropBet.tsx
@@ -1,42 +1,78 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from 'react';
 
 const AddPropBet = () => {
+	const [propBetFields, setPropBetFields] = useState({
+		description: '',
+		shortDescription: '',
+		sideOne: '',
+		sideTwo: '',
+		notes: ''
+	});
+
+	const [isValid, setIsValid] = useState(false);
+
+	const handleFieldChange = (fieldName: 'description' | 'shortDescription' | 'sideOne' | 'sideTwo' | 'notes', newValue: string) => {
+		let newPropBetFields = { ...propBetFields };
+		newPropBetFields[fieldName] = newValue;
+
+		setPropBetFields(newPropBetFields);
+
+		if (newPropBetFields.description !== '' && newPropBetFields.shortDescription !== '' && newPropBetFields.sideOne !== '' && newPropBetFields.sideTwo !== '') {
+			setIsValid(true);
+		}
+		else {
+			setIsValid(false);
+		}
+	};
+
 	return (
 		<Form>
 			<Form.Label htmlFor="description">Description</Form.Label>
 			<Form.Control
 				type="text"
 				id="description"
+				value={propBetFields.description}
+				onChange={(event) => handleFieldChange.call(null, 'description', event.target.value)}
 			/>
 
 			<Form.Label htmlFor="shortDescription">Short Description</Form.Label>
 			<Form.Control
 				type="text"
 				id="shortDescription"
+				value={propBetFields.shortDescription}
+				onChange={(event) => handleFieldChange.call(null, 'shortDescription', event.target.value)}
 			/>
 
 			<Form.Label htmlFor="sideOne">Side One</Form.Label>
 			<Form.Control
 				type="text"
 				id="sideOne"
+				value={propBetFields.sideOne}
+				onChange={(event) => handleFieldChange.call(null, 'sideOne', event.target.value)}
 			/>
 
 			<Form.Label htmlFor="sideTwo">Side Two</Form.Label>
 			<Form.Control
 				type="text"
 				id="sideTwo"
+				value={propBetFields.sideTwo}
+				onChange={(event) => handleFieldChange.call(null, 'sideTwo', event.target.value)}
 			/>
 
 			<Form.Label htmlFor="notes">Notes</Form.Label>
 			<Form.Control
 				as="textarea"
 				id="notes"
+				value={propBetFields.notes}
+				onChange={(event) => handleFieldChange.call(null, 'notes', event.target.value)}
 			/>
 
 			<Button
 				variant="primary"
 				type="submit"
+				disabled={!isValid}
 			>Add Prop Bet</Button>
 		</Form>
 	);
