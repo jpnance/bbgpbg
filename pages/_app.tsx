626f1fdbe81456ba5e9499a929f6bb5fd771beb9
Patrick Nance <jpnance@gmail.com>
2023-01-24 13:26:02 -0800

    Adding the actual Bootstrap CSS into the fray.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
+import '@/styles/global.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
